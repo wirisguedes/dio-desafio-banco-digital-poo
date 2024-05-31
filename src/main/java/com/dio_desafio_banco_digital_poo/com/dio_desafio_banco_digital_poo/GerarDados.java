12d12cd57795fcf770c253692250ec2b4599b30b
wirisguedes <wirisguedes@gmail.com>
2024-05-31 16:57:16 -0300

    implementacao estrutura projeto

---
+++ src/main/java/com/dio_desafio_banco_digital_poo/com/dio_desafio_banco_digital_poo/GerarDados.java
@@ -0,0 +1,13 @@
+package com.dio_desafio_banco_digital_poo.com.dio_desafio_banco_digital_poo;
+
+import java.util.Locale;
+
+import net.datafaker.Faker;
+
+public class GerarDados {
+	
+	static Faker faker = new Faker(new Locale("pt-BR"));
+
+	
+   
+}
